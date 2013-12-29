c21b4bc238ffe4604571d86d8c2e8864ab85b671
Blake Bourque <Techplex.Engineer@gmail.com>
2013-12-28 20:16:00 -0500

    oops

--- server/methods/i2cset.js
+++ server/methods/i2cset.js
@@ -17,16 +17,16 @@ Meteor.methods({
 
 		message |= args.zone;
 
-		// child = exec('/usr/sbin/i2cset -y 1 0x04 0x'+message.toString(16), //
-		// function (error, stdout, stderr) {
-		// 	// console.log('stdout: ' + stdout);
-		// 	// console.log('stderr: ' + stderr);
-		// 	if (error !== null) {
-		// 		console.log('exec error: ' + error);
-		// 	}
-		// 	fut['return'](error||0);
-		// });
+		child = exec('/usr/sbin/i2cset -y 1 0x04 0x'+message.toString(16), //
+		function (error, stdout, stderr) {
+			// console.log('stdout: ' + stdout);
+			// console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+			fut['return'](error||0);
+		});
 
-		// return fut.wait();
+		return fut.wait();
 	}
 })
